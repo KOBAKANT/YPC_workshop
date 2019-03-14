ab9eae53ea81d2b37d53e865342a19c612339a5d
mikst <mika@kobakant.at>
2019-03-14 14:17:02 +0100

    test

--- EthanOliverthenotsowonkyrainbow_pause/Olivers_arcade_light_game_/Olivers_arcade_light_game_.ino
+++ EthanOliverthenotsowonkyrainbow_pause/Olivers_arcade_light_game_/Olivers_arcade_light_game_.ino
@@ -6,10 +6,12 @@
  */
  
 //Try to stop it on green (or what ever you want) :)
+//
 // give it a name:
 int led = 13;
 
 boolean switch1;
+boolean green_led;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -25,13 +27,13 @@ void setup() {
    pinMode(10,INPUT);
 pinMode(2,OUTPUT);
    Serial.begin(9600);
+   
   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 digitalWrite(2,HIGH);
-
 switch1=digitalRead(10);
   if(switch1==0){
   digitalWrite(3, HIGH);
@@ -51,12 +53,14 @@ switch1=digitalRead(10);
   switch1=digitalRead(10);
   if(switch1==0){
     digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);  
+  digitalWrite(6, HIGH);
+  green_led=1;  
   delay(70);
   }
   switch1=digitalRead(10);
   if(switch1==0){
   digitalWrite(6, LOW);
+  green_led=0;
   digitalWrite(7, HIGH);
   delay(70);
   }
@@ -108,6 +112,18 @@ switch1=digitalRead(10);
   digitalWrite(3,HIGH);
   delay(70);
   }
-  
+  Serial.println(green_led);
+  if(switch1==1){
+    if(green_led==1){
+      digitalWrite(2,LOW);
+      delay(50);
+      digitalWrite(2,HIGH);
+      delay(100);
+      digitalWrite(2,LOW);
+      delay(50);
+      digitalWrite(2,HIGH);
+      delay(100);
+    }
+  }
   // wait for a second
 }
